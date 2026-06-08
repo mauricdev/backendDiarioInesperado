a0a860da980d9133446ccab630bae8bd448ed655
mauricdev <49356191+mauricdev@users.noreply.github.com>
2026-06-08 02:30:46 -0400

    feat: initialize NestJS application and configure CORS policies

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // <-- Habilitar CORS
-  await app.listen(process.env.PORT ?? 3000);
+
+  // HABILITAR CORS AQUÍ
+  app.enableCors({
+    origin: [
+      'https://eldiarioinesperado.cl',
+      'https://www.eldiarioinesperado.cl',
+      'http://localhost:4200' // Por si usas Angular localmente
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
